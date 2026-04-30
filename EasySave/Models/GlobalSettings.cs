ea30cb05322aed5a15007bf18fdec8591bfef087
Eloïse <Eose@THINKPAD-ELO>
2026-04-30 09:56:01 +0200

    Refonte du SettingsManager et ajout de GlobalSettings
    - Création du modèle GlobalSettings pour centraliser toutes les préférences.
    - Mise à jour du JsonSettingsRepository pour charger/sauvegarder l'objet global.
    - Ajout de la gestion du format des logs (JSON/XML) et des extensions à chiffrer dans le SettingsManager.
    - Suppression de l'ancien ProcessWatcher (doublon).

---
+++ EasySave/Models/GlobalSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Models
+{
+    public class GlobalSettings
+    {
+        public List<SettingsProcess> BusinessProcesses { get; set; } = new List<SettingsProcess>();
+        public string LogFormat { get; set; } = "JSON";
+        public List<string> EncryptedExtensions { get; set; } = new List<string>();
+    }
+}
