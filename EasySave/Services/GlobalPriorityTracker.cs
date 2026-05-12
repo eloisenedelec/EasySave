d55b57fb1eae5298090c88ddc1ca277cf720083e
Eloïse <Eose@THINKPAD-ELO>
2026-05-12 11:24:43 +0200

    implémentation du moteur de règles (priorités et fichiers volumineux)
    - Création du GlobalPriorityTracker : implémentation d'un verrou global via ManualResetEventSlim.
         Garantit qu'aucun fichier normal n'est copié tant qu'un fichier prioritaire est en attente dans n'importe quel job.
    - Création du LargeFileCoordinator : utilisation d'un sémaphore (SemaphoreSlim) pour limiter
         à un seul transfert simultané les fichiers dépassant le seuil de taille (n Ko).
    Mise à jour de GlobalSettings.cs avec les nouveaux paramètres (extensions prioritaires et seuil de taille).
    - Complétion du SettingsManager avec les méthodes de gestion (Add/Remove/Get) pour ces nouveaux réglages.

---
+++ EasySave/Services/GlobalPriorityTracker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Services
+{
+    public class GlobalPriorityTracker
+    {
+        private static readonly Lazy<GlobalPriorityTracker> _instance = new Lazy<GlobalPriorityTracker>(() => new GlobalPriorityTracker());
+        public static GlobalPriorityTracker Instance => _instance.Value;
+        private int _priorityFilesRemaining = 0;
+        private readonly ManualResetEventSlim _priorityWaitHandle = new ManualResetEventSlim(true);
+        private readonly object _lock = new object();
+
+        public void AddPriorityFiles(int count)
+        {
+            if (count <= 0) return;
+
+            lock (_lock)
+            {
+                _priorityFilesRemaining += count;
+                _priorityWaitHandle.Reset();
+            }
+        }
+
+        public void PriorityFileFinished()
+        {
+            lock (_lock)
+            {
+                if (_priorityFilesRemaining > 0)
+                {
+                    _priorityFilesRemaining--;
+
+                    if (_priorityFilesRemaining == 0)
+                    {
+                        _priorityWaitHandle.Set();
+                    }
+                }
+            }
+        }
+
+        public void WaitForPriorityIfNeeded()
+        {
+            _priorityWaitHandle.Wait();
+        }
+    }
+}
